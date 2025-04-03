cefb4593c704e2ab4cf9fe56b3f97b84828d65f0
Sergey <80700865+si-network@users.noreply.github.com>
2025-04-03 19:41:59 +0300

    fix moved   comments

--- longestCommonPrefix.go
+++ longestCommonPrefix.go
@@ -5,19 +5,19 @@ package algorithms
 // If there is no common prefix, return an empty string ""
 
 func longestCommonPrefix(strs []string) string {
-	var prefix string												// работаем с одной переменной
+	var prefix string										// работаем с одной переменной
 
-	if len(strs[0]) > 0 {											// если первая строка не пустая, то выполняем следующий код
-		prefix = strs[0]											// присваиваем переменной prefix первую строку среза strs
+	if len(strs[0]) > 0 {										// если первая строка не пустая, то выполняем следующий код
+		prefix = strs[0]									// присваиваем переменной prefix первую строку среза strs
 
-		for idx := 1; idx < len(strs); idx++ {						// начинаем обход строк strs, начиная со второй, idx хранит индекс строки в срезе strs
-			for symbol := 0; symbol < len(prefix); symbol++ {		// проход по каждому символу только первой строки
+		for idx := 1; idx < len(strs); idx++ {							// начинаем обход строк strs, начиная со второй, idx хранит индекс строки в срезе strs
+			for symbol := 0; symbol < len(prefix); symbol++ {				// проход по каждому символу только первой строки
 				if len(strs[idx]) > symbol && strs[idx][symbol] == prefix[symbol] { 	// сравниваем symbol-ый символ первой строки с symbol-ым символом strs[idx] строки
-					symbol++										// если равны то переходим к следующему символу
-					continue					 					// и продолжаем цикл
+					symbol++							// если равны то переходим к следующему символу
+					continue					 		// и продолжаем цикл
 				}
-				prefix = prefix[:symbol]							// если не равны, то обновляем prefix на совпадающий префикс
-				break;			 									// и переходим к вледующей строке 
+				prefix = prefix[:symbol]						// если не равны, то обновляем prefix на совпадающий префикс
+				break;			 						// и переходим к вледующей строке 
 			}
 			if len(prefix) == 0 { break } 							// если общий префикс сравниваемых строк не найден, то выходим из цикла, т.к. дальнейший обход по strs не имеет смысла
 		}
